00f49ecdbcf0039710fbe90cad1b7b97c8c787e8
HoangHuuy <hoanghuy020420@gmail.com>
2023-06-07 14:12:55 +0700

    add ui bootstrap, auth, connect db

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,25 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-$hello = "hello";
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::controller(UserController::class)->group(function () {
+    Route::prefix('users')->group(function () {
+        Route::get('/add', 'toAddPage');
+        Route::post('/add', 'add');
+    });
 });
 
-Route::get('/user/{id}', function (string $id) {
-    // Giả sử hệ thống có 5 user với id từ 1 đến 5
-    // thông tin bao gồm tên, email, số điện thoại, ...
-    $user1 = array("id" => 1, "name" => "Jack", "mail" => "jack99@gmail.com", "phone number" => "09999999999");
-    $user2 = array(2, "Noel", "noel.hudges@gmail.com", "09999669966");
-    $user3 = array(3, "York", "york-yoru@gmail.com", "099997777777");
-    $user4 = array(4, "Thorfinn", "thorfinn@gmail.com", "01689999999");
-    $user5 = array(5, "Luxiang", "Luxiang@gmail.com", "03869999565");
 
-    dd($user1->id);
-
-    return 'User '.$id;
-    // Regular Expression Constraint
-})->where('id', '[0-9]+');
-
-
+Auth::routes();
